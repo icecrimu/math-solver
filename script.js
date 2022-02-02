44561fe541ef07ab2989cc18c829267419df378a
icecrimu <charles.banigon@gmail.com>
2022-02-02 17:48:24 +0800

    add MULTIPLY_DIVIDE_REGEX

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const form = document.querySelector("#equation-form")
 const inputElement = document.querySelector("#equation")
 const outputElement = document.querySelector("#results")
 
+const MULTIPLY_DIVIDE_REGEX =
+  /(?<operand1>\d+)\s*(?<operation>[\*\/])\s*(?<operand2>\d+)/
+
 form.addEventListener("submit", e => {
   e.preventDefault()
 
